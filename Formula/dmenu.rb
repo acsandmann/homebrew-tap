da716438420668d40cf57bb5ef793d08e0ea5919
GitHub Action <action@github.com>
2025-06-28 05:32:25 +0000

    Update dmenu to 0.0.8

--- Formula/dmenu.rb
+++ Formula/dmenu.rb
@@ -2,8 +2,8 @@ class Dmenu < Formula
   desc "Dynamic menu for macOS, inspired by dmenu for X11"
   homepage "https://github.com/acsandmann/dmenu"
 
-  url "https://api.github.com/repos/acsandmann/dmenu/tarball/0.0.7"
-  sha256 "4cd303b5144683269d58ff3108dfaed4f09df92519fc8d95f329ef577a78f24a"
+  url "https://api.github.com/repos/acsandmann/dmenu/tarball/0.0.8"
+  sha256 "1b9b4531567f848613a59fcebb6171b0d60220f7443242b5eef1bf48f3fa1036"
   license "MIT"
   head "https://github.com/acsandmann/dmenu.git", branch: "main"
 
