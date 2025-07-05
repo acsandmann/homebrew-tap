a3fefdea220bc91d54236626afbff9396c80d667
GitHub Action <action@github.com>
2025-07-05 22:57:20 +0000

    Update menuanywhere to 1.0.4

--- Formula/menuanywhere.rb
+++ Formula/menuanywhere.rb
@@ -1,9 +1,9 @@
 class Menuanywhere < Formula
   desc "the macos menu bar at your fingertips"
   homepage "https://github.com/acsandmann/menuanywhere"
-  url "https://api.github.com/repos/acsandmann/menuanywhere/tarball/1.0.3"
-  sha256 "c0c08e93ef5094a478b9916f52f1fe7818c41c9180ca2b7e12aceab379bceb74"
-  version "1.0.3"
+  url "https://api.github.com/repos/acsandmann/menuanywhere/tarball/1.0.4"
+  sha256 "b16fcf0e2df31df5c77245a06ac3ddb2eb0ba71521edc528889913a7f39dd943"
+  version "1.0.4"
   license "MIT"
 
   depends_on xcode: ["12.0", :build]
