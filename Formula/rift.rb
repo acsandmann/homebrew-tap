fcc9c443c011168b5b6bad25da98d65958163053
acsandmann <157552025+acsandmann@users.noreply.github.com>
2025-10-14 20:58:47 -0400

    Update rift.rb

--- Formula/rift.rb
+++ Formula/rift.rb
@@ -1,7 +1,7 @@
 class Rift < Formula
   desc "Tiling window manager for macOS"
   homepage "https://github.com/acsandmann/rift"
-  version "0.0.1"
+  version "0.0.0"
   url "https://github.com/acsandmann/rift/releases/download/v#{version}/rift-universal-macos-#{version}.tar.gz"
   sha256 "6dab385c63e8bfa9fd428296a50585062657e49bd458574335a6ac3f761d519e"
   license "Apache-2.0"
